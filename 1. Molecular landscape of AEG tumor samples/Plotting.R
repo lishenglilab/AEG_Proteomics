7801eb2bd4711af9eb6cff1639311a437b6d347a
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-07 07:31:13 +0800

    Create Plotting.R

---
+++ 1. Molecular landscape of AEG tumor samples/Plotting.R
@@ -0,0 +1,9 @@
+### waterfall plot of top mutation cancer genes
+library(maftools)
+setwd('/home/shengli/projects/AEG_proteomics/data/WES')
+aeg <- read.maf(maf='AEG_mutation_cancer_gene.maf')
+pdf('/home/shengli/projects/AEG_proteomics/figures/WES/mutation_waterfall_cancer_genes_top30.pdf',height=6,width=10)
+oncoplot(maf=aeg,draw_titv=T,top=30,showTumorSampleBarcodes=F)
+dev.off()
+
+###
