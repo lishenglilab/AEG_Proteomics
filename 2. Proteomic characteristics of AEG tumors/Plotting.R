67d465b474ea3ae3a1eec097d82ba97f73f42231
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-14 06:40:00 +0800

    Update Plotting.R

--- 2. Proteomic characteristics of AEG tumors/Plotting.R
+++ 2. Proteomic characteristics of AEG tumors/Plotting.R
@@ -1,15 +1,5 @@
 ### Volcano plot showing the difference in proteins between AEG tumor and paired NAT samples. Fig. 2a
-## calculate the difference
-library(limma) # version 3.46.0
-setwd('/home/shengli/projects/AEG_proteomics/data/proteome')
-prot_mx <- read.table('Proteomics_iBAQ103_log2quantile_normlization_impute_perc25.txt',header=T,row.names=1,sep='\t')
-design_nt <- cbind(Intercept=1,
-                   Group=c(rep(1,103),rep(0,103)))
-fit <- lmFit(prot_mx,design_nt)
-fit <- eBayes(fit,trend=TRUE)
-dep_nt <- topTable(fit,coef=2,number=Inf)
-setwd('/home/shengli/projects/AEG_proteomics/results/proteome')
-write.table(dep_nt,file="Normal_Tumor_DEP_all.txt",quote=F,sep='\t',append=F)
+
 
 ## valcono plot of differentially expressed proteins
 rm(list=ls())
