29143b47b009db9a68170a908aabbeb6680b4a54
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-17 15:28:38 +0800

    Update Plotting.R

--- 2. Proteomic characteristics of AEG tumors/Plotting.R
+++ 2. Proteomic characteristics of AEG tumors/Plotting.R
@@ -43,27 +43,6 @@ barplot(enrich_4bar[,'lgp'],horiz=T,names.arg=rownames(enrich_4bar),las=2,xlim=c
 dev.off()
 
 ## down-regulated proteins
-rm(list = ls())
-options(stringsAsFactors = FALSE) # prohibit shift form character to factor
-library(tidyverse)
-library(Seurat)
-library(clusterProfiler)
-library(org.Hs.eg.db)
-
-setwd('/home/shengli/projects/AEG_proteomics/results/proteome')
-dep_df <- read.table('Normal_Tumor_DEP_sig_genes.txt',header=T,row.names=1,sep='\t')
-down_df <- dep_df[which(dep_df[,'logFC']<0),]
-degID <- bitr(down_df$Gene, fromType = "SYMBOL", toType = c( "ENTREZID" ), OrgDb = org.Hs.eg.db ) # shift gene symbol to entrez id
-enrich <- enrichKEGG(gene =degID$ENTREZID ,
-                     organism = 'hsa', 
-                     pvalueCutoff=1, qvalueCutoff=1) # use OrgDb fitted for different species
-GeneRatio <- as.numeric(lapply(strsplit(enrich$GeneRatio,split="/"),function(x) as.numeric(x[1])/as.numeric(x[2])))
-BgRatio <- as.numeric(lapply(strsplit(enrich$BgRatio,split="/"),function(x) as.numeric(x[1])/as.numeric(x[2])  ))
-enrich_factor <- GeneRatio/BgRatio
-out <- data.frame(enrich$ID,enrich$Description,enrich$GeneRatio,enrich$BgRatio,round(enrich_factor,2),enrich$pvalue, enrich$p.adjust,enrich$qvalue,enrich$geneID)
-colnames(out) <- c("ID","Description","GeneRatio","BgRatio","enrich_factor","pvalue", "p.adjust","qvalue","geneID")
-setwd("/home/shengli/projects/AEG_proteomics/results/proteome")
-write.table(out, "KEGG_Prot_diff_down.txt", row.names = F, sep="\t", quote = F)        
 
 ## barplot
 setwd('/home/shengli/projects/AEG_proteomics/results/proteome')
