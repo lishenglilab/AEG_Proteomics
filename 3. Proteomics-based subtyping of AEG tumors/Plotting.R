2b07e8566ab584c37c0aac99f752c48e3cb5c485
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-17 16:09:10 +0800

    Update Plotting.R

--- 3. Proteomics-based subtyping of AEG tumors/Plotting.R
+++ 3. Proteomics-based subtyping of AEG tumors/Plotting.R
@@ -30,4 +30,42 @@ ggsurvplot(
 )
 dev.off()
 
+# plot significant mutations between subtypes
+rm(list=ls())
+library(ggplot2)
+library(ggrepel)
+setwd('/home/shengli/projects/AEG_proteomics/results/WES')
+enrich <- read.table('subgroups_mut_sig.txt',header=T,sep='\t',row.names=1)
+enrich$lgp <- -log10(enrich[,'Pvalue'])
+enrich[which(enrich[,'OR'] == 0),'OR'] <- 1
+enrich$logor <- log2(enrich[,'OR'])
+enrich$gene <- row.names(enrich)
+enrich_none <- enrich[which(enrich[,'Group'] == 'Group0'),]
+enrich_g1 <- enrich[which(enrich[,'Group'] == 'Group1'),]
+enrich_g2 <- enrich[which(enrich[,'Group'] == 'Group2'),]
+enrich_g3 <- enrich[which(enrich[,'Group'] == 'Group3'),]
+g1_gene <- as.character(row.names(enrich_g1))
+g2_gene <- as.character(row.names(enrich_g2))
+g3_gene <- as.character(row.names(enrich_g3))
+freq <- read.table('AEG_groups_mutation.txt',header=T,sep='\t')
+freq_g1 <- freq[which(freq[,'Gene'] %in% g1_gene),]
+freq_g2 <- freq[which(freq[,'Gene'] %in% g2_gene),]
+freq_g3 <- freq[which(freq[,'Gene'] %in% g3_gene),]
+enrich_g1$freq <- freq_g1[,'Group1']
+enrich_g2$freq <- freq_g2[,'Group2']
+enrich_g3$freq <- freq_g3[,'Group3']
+pdf('/home/shengli/projects/AEG_proteomics/figures/WES/AEG_mutation_enrich_groups.pdf',width=6.87,height=5.92)
+ggplot(enrich,aes(x=logor,y=lgp,label=gene)) +
+  geom_point(data=enrich_none,aes(x=logor,y=lgp),color='lightgray') +
+  geom_point(data=enrich_g1,aes(x=logor,y=lgp,size=freq),color='red') +
+  geom_point(data=enrich_g2,aes(x=logor,y=lgp,size=freq),color='blue') +
+  geom_point(data=enrich_g3,aes(x=logor,y=lgp,size=freq),color='green') +
+  geom_label_repel(data = subset(enrich, lgp > 2 & abs(logor) > 2),
+                   size = 3,
+                   box.padding = 0.5,
+                   point.padding = 0.5,
+                   segment.size = 0.2,
+                   segment.color = "grey50") +
+  theme(panel.background=element_rect(colour="black",fill="white"))
+dev.off()
 
