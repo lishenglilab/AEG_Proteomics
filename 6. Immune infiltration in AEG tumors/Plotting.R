3e0c16da25e13402f4b9fc3b5fa8c68b22fb8d73
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-18 12:55:21 +0800

    Update Plotting.R

--- 6. Immune infiltration in AEG tumors/Plotting.R
+++ 6. Immune infiltration in AEG tumors/Plotting.R
@@ -1,10 +1,10 @@
 ### plot differences of immune cells, Fig. 6b
 library(ggplot2)
 setwd('/home/shengli/projects/AEG_proteomics/results/RNAseq')
-immune_dif <- read.table('Immune_infil_difference_df.txt',header=T,sep='\t')
+immune_dif <- read.table('Immune_infil_xCell_difference_df.txt',header=T,sep='\t')
 immune_dif$lgp <- -log10(immune_dif[,'Pvalue'])
 immune_cells <- immune_dif[,'Immune_cell']
-pdf('/home/shengli/projects/AEG_proteomics/figures/RNAseq/Immune_infil_diff.pdf',width=9.5,height=3.5)
+pdf('/home/shengli/projects/AEG_proteomics/figures/RNAseq/Immune_infil_xCell_diff.pdf',width=9.5,height=3.5)
 ggplot(immune_dif,aes(x=Immune_cell,y=Group))+
   geom_point(aes(size=lgp,col=Difference))+
   scale_color_gradient2(low="blue",high="red",na.value="white",name="Difference")+
@@ -24,9 +24,8 @@ dev.off()
 # plot individual immune cells, Fig. 6c-d, Supplementary Fig. 14
 library(ggplot2)
 setwd('/home/shengli/projects/AEG_proteomics/results/RNAseq')
-immune_prof <- read.table('Immune_CIBERSORT.txt',header=T,sep='\t')
-immune_prof <- t(immune_prof)
-immune_samples <- immune_prof['Sample',]
+immune_prof <- read.table('xCell_AEG_gene_tmp_mx_ordered_xCell_2052121421.txt',header=T,row.names=1,sep='\t')
+immune_samples <- colnames(immune_prof)
 samples_t1 <- c('C551594','C559459','C560016','C563560','C564447','C569091','C575352','C579894','C582702','C584162','C586163','C583851','C590054','C595498',
                 'C596146','C1005614','C1006005','C1009828','C532443','C505919','C562401','C587488','C599521','C584104','C498355','C565023','C246567',
                 'C576092','C488963','C533179','C504861','C568971','C528365','C533153','C1007477','C572176','C594667','C522528','C561723','C584243')
@@ -49,8 +48,7 @@ samples_n3 <- c('N332654','N386485','N266473','N338321','N338421','N335207','N37
                 'N210038','N260824','N218213','N391965','N363245','N196436','N321599','N241209','N215502','N248148','N322712','N248917','N0600290','N291416',
                 'N1004644','N305692','N258083','N259302','N1003009','N288415','N314628','N260319','N199181','N256818','N260513','N260742')
 samples_n3_p <- samples_n3[which(samples_n3 %in% immune_samples)]
-colnames(immune_prof) <- immune_prof['Sample',]
-cell <- c('Macrophages.M2')
+cell <- c('aDC')
 s1_normal = as.numeric(immune_prof[cell,samples_n1_p])
 s1_tumor = as.numeric(immune_prof[cell,samples_t1_p])
 s2_normal = as.numeric(immune_prof[cell,samples_n2_p])
@@ -61,7 +59,7 @@ cell_abundance <- c(s1_normal,s1_tumor,s2_normal,s2_tumor,s3_normal,s3_tumor)
 cell_abund <- data.frame(Abundance = cell_abundance,
                          Group = c(rep('S1_normal',length(s1_normal)),rep('S1_tumor',length(s1_tumor)),rep('S2_normal',length(s2_normal)),
                                    rep('S2_tumor',length(s2_tumor)),rep('S3_normal',length(s3_normal)),rep('S3_tumor',length(s3_tumor))))
-pdf('/home/shengli/projects/AEG_proteomics/figures/Proteomics/Macrophage_M2_comparison_3subtypes.pdf',height=5,width=6)
+pdf('/home/shengli/projects/AEG_proteomics/figures/RNAseq/xCell_cell_abundance_aDC.pdf',height=5,width=6)
 ggplot(cell_abund,aes(x=Group,y=Abundance)) +
   geom_jitter(width=0.15,size=0.7) +
   geom_boxplot(color="black",fill=NA,outlier.shape=NA,width=0.3) +
@@ -76,3 +74,4 @@ ggplot(cell_abund,aes(x=Group,y=Abundance)) +
         strip.background = element_blank(),
         strip.text.x = element_text(color="black",size=7,vjust=0))
 dev.off()
+
