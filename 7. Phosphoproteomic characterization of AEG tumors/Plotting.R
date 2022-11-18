bcbc1be68045c2c790ee4e5c5e51b919ddb4f5db
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-18 13:01:11 +0800

    Update Plotting.R

--- 7. Phosphoproteomic characterization of AEG tumors/Plotting.R
+++ 7. Phosphoproteomic characterization of AEG tumors/Plotting.R
@@ -1,1 +1,12 @@
-
+### phosphorylation abundance distribution
+rm(list=ls())
+library(ggplot2)
+setwd('/home/shengli/projects/AEG_proteomics/data/phosphoproteome')
+prot_ibaq <- read.table('Phosphoproteomics_iBAQ103_log2quantile_normlization_df.txt',header=T)
+sample_cancer <- as.character(unique(prot_ibaq[which(prot_ibaq[,'Group']=='Cancer'),'Sample']))
+sample_normal <- as.character(unique(prot_ibaq[which(prot_ibaq[,'Group']=='Normal'),'Sample']))
+samples <- c(sample_cancer,sample_normal)
+pdf('/home/shengli/projects/AEG_proteomics/figures/Phosphoproteomics_iBAQ_norm_distribution.pdf',width=20,height=5)
+ggplot(prot_ibaq,aes(x=Sample,y=Abundance,fill=Group)) +
+  geom_boxplot()
+dev.off()
