b57d8e0d121bc2ca9957a37226c5415773c20110
lishenglilab <76990733+lishenglilab@users.noreply.github.com>
2022-11-18 12:59:41 +0800

    Update Analysis.R

--- 7. Phosphoproteomic characterization of AEG tumors/Analysis.R
+++ 7. Phosphoproteomic characterization of AEG tumors/Analysis.R
@@ -1,1 +1,10 @@
+### normalization
+library(limma)
+setwd('/home/shengli/projects/AEG_proteomics/data/phosphoproteome')
+prot_ibaq <- read.table('Phosphoproteomics_iBAQ103.txt',header=T,row.names=1)
+prot_ibaq <- as.matrix(prot_ibaq)
+quant_norm_ibaq <- normalizeQuantiles(prot_ibaq)
+write.table(quant_norm_ibaq,file='Phosphoproteomics_iBAQ103_quantile_normlization.txt',quote=F,sep='\t')
+log2_norm_ibaq <- log2(quant_norm_ibaq)
+write.table(log2_norm_ibaq,file='Phosphoproteomics_iBAQ103_log2quantile_normlization.txt',quote=F,sep='\t')
 
